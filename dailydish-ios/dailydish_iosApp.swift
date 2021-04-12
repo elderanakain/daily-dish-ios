e2c672ccfa7e5cf4aa13fc8c1c3f6a5d4697cff2
Arcadii Rubailo <rubailo.arcadii@gmail.com>
2021-04-12 15:25:02 +0200

    Init

---
+++ dailydish-ios/dailydish_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  dailydish_iosApp.swift
+//  dailydish-ios
+//
+//  Created by Arcadii Rubailo on 12/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct dailydish_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
